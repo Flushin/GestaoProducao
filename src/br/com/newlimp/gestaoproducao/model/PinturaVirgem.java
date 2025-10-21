a8ebbb836e4a6889a363e325cdb180939c88444d
Lucas Silva <lcsbetel@hotmail.com>
2025-10-21 08:13:53 -0300

    feat: Todos os métodos da Classe PinturaVirgem desenvolvidos.

--- src/br/com/newlimp/gestaoproducao/model/PinturaVirgem.java
+++ src/br/com/newlimp/gestaoproducao/model/PinturaVirgem.java
@@ -1,6 +1,7 @@
 package br.com.newlimp.gestaoproducao.model;
 
 import br.com.newlimp.gestaoproducao.model.enums.Cores;
+import br.com.newlimp.gestaoproducao.model.enums.StatusProcesso;
 
 import java.util.List;
 import java.util.Map;
@@ -17,16 +18,49 @@ public class PinturaVirgem extends Processo{
     public void executar() {
         //irá iniciar o processo com uma quantidade padrão de todos os insumos
         //no final do processo a pessoa diz o que usou
+        if (!verificarEstoque()) { //aqui é sempre testando se é false. Se for, entra no if.
+            System.out.println("Estoque insuficiente para iniciar o processo de pintura virgem.");
+            return;
+        }
+
+        this.setStatus(StatusProcesso.EM_EXECUCAO);
+        System.out.println("Processo de pintura virgem iniciado com cor: " + cor);
+        consumirRecursos();
     }
 
     public void registrarProducao(int quantidade) {
+        this.chapasPintadas += quantidade;
+
+        if (chapasPintadas >= chapasPlanejadas) {
+            this.setStatus(StatusProcesso.CONCLUIDO);
+            System.out.println("Processo de pintura concluído com sucesso!");
+        } else {
+            System.out.println("Produção parcial registrada: " + chapasPintadas + " chapas pintadas.");
+        }
     }
 
     public boolean verificarEstoque() {
-       return true;
+       if (tintaUtilizada.getVolumeRestante() <= 0) {
+           System.out.println("Sem tinta suficiente para pintura");
+           return false;
+       }
+
+       if (chapasUsadas == null || chapasUsadas.isEmpty()) {
+           System.out.println("Nenhuma chapa disponível para pintura.");
+           return false;
+       }
+
+       return true; //Entao tem estoque, retorna true que significa que tem estoque.
     }
 
     public void consumirRecursos() {
+        double consumoPorChapa = 0.5; //exemplo: 0.5 litros por chapa
+        double consumoTotal = chapasPlanejadas * consumoPorChapa; //2.5
 
+        tintaUtilizada.setVolumeRestante(
+                tintaUtilizada.getVolumeRestante() - consumoTotal
+        );
+
+        System.out.println(consumoTotal + "L de tinta " + tintaUtilizada.getCor() + " consumidos.");
     }
 }
