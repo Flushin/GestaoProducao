0e877b60615688290e23f09b4be75a69a626aaa2
Lucas Silva <lcsbetel@hotmail.com>
2025-10-22 08:36:33 -0300

    feat: Getters and Setters.

--- src/br/com/newlimp/gestaoproducao/model/Processo.java
+++ src/br/com/newlimp/gestaoproducao/model/Processo.java
@@ -5,12 +5,56 @@ import br.com.newlimp.gestaoproducao.model.enums.StatusProcesso;
 import java.time.LocalDate;
 
 public class Processo {
-    private Long id;
+    private int id;
     private String nome;
     private String responsavel;
     private LocalDate dataProgramacao;
     private StatusProcesso status;
     private double quantidadeProduzida;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public LocalDate getDataProgramacao() {
+        return dataProgramacao;
+    }
+
+    public void setDataProgramacao(LocalDate dataProgramacao) {
+        this.dataProgramacao = dataProgramacao;
+    }
+
+    public StatusProcesso getStatus() {
+        return this.status;
+    }
+
+    public double getQuantidadeProduzida() {
+        return quantidadeProduzida;
+    }
+
+    public void setQuantidadeProduzida(double quantidadeProduzida) {
+        this.quantidadeProduzida = quantidadeProduzida;
+    }
 
     public void iniciarProcesso() {
         this.status = StatusProcesso.EM_EXECUCAO;
