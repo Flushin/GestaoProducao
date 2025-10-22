0d1053ee5720ba73ee0968193248d2dc3c47d8f8
Lucas Silva <lcsbetel@hotmail.com>
2025-10-22 08:36:44 -0300

    feat: Troca de primitivo para ENUM.

--- src/br/com/newlimp/gestaoproducao/model/Tinta.java
+++ src/br/com/newlimp/gestaoproducao/model/Tinta.java
@@ -1,7 +1,9 @@
 package br.com.newlimp.gestaoproducao.model;
 
+import br.com.newlimp.gestaoproducao.model.enums.Cores;
+
 public class Tinta extends ItemEstoque {
-    private String cor;
+    private Cores cor;
     private double volumeTotal;
     private double volumeRestante;
 
@@ -10,10 +12,22 @@ public class Tinta extends ItemEstoque {
     }
 
     public void setVolumeRestante(double volumeRestante) {
-        this.volumeRestante -= volumeRestante;
+        this.volumeRestante = this.volumeTotal - this.volumeRestante;
     }
 
-    public String getCor() {
+    public Cores getCor() {
         return this.cor;
+    }
+
+    public void setCor(Cores cor) {
+        this.cor = cor;
+    }
+
+    public double getVolumeTotal() {
+        return this.volumeTotal;
+    }
+
+    public void setVolumeTotal(double volumeTotal) {
+        this.volumeTotal = volumeTotal;
     }
 }
