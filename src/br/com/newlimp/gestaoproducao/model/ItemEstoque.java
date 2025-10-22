a4cce4ad2b70a27cbfb2ce1c5428763af852a493
Lucas Silva <lcsbetel@hotmail.com>
2025-10-22 08:36:14 -0300

    feat: Getters and Setters.

--- src/br/com/newlimp/gestaoproducao/model/ItemEstoque.java
+++ src/br/com/newlimp/gestaoproducao/model/ItemEstoque.java
@@ -4,6 +4,46 @@ public class ItemEstoque {
     private String nome;
     private double quantidade;
     private String medida;
-    private Long id;
+    private int id;
     private boolean emUso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isEmUso() {
+        return emUso;
+    }
+
+    public void setEmUso(boolean emUso) {
+        this.emUso = emUso;
+    }
 }
